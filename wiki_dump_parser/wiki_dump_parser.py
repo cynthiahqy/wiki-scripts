31d9d04305bf2cf4ee4bdc64090970fb4190853f
cynthiahqy <29718979+cynthiahqy@users.noreply.github.com>
2019-08-01 12:13:37 +1000

    Change handling of missing fields
    Alter code to write row to csv AND print row to console

--- wiki_dump_parser/wiki_dump_parser.py
+++ wiki_dump_parser/wiki_dump_parser.py
@@ -110,12 +110,12 @@ def xml_to_csv(filename):
                       contributor_id,contributor_name,comment,
                       bytes_var]
 
-      # Do not print (skip) revisions that has any of the fields not available
+      # print rows with empty fields
       if not has_empty_field(revision_row):
         output_csv.write(csv_separator.join(revision_row) + '\n')
       else:
-        print("The following line has imcomplete info and therefore it's been removed from the dataset:")
         print(revision_row)
+        output_csv.write(csv_separator.join(revision_row) + '\n')
 
       # Debug lines to standard output
       if Debug:
